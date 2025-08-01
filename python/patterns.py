652b81c6c9e733561baeee1f0a66e34f87279988
vishnudev3154 <vishnudevjayakumar54@gmail.com>
2025-08-01 09:08:58 +0530

    'whileloop'

---
+++ python/patterns.py
@@ -0,0 +1,17 @@
+# for i in range(3):
+#     for j in range(3):    
+#         print('*' ,end='')
+#     print()
+
+# for i in range(1,5):
+  
+#     for j in range(1,5):    
+#         print(j ,end='')
+#     print()
+
+for i in range(1,5):
+  
+    for j in range(1,5):    
+        print(i ,end='')
+    print()
+
---
+++ python/whileeven.py
@@ -0,0 +1,7 @@
+a=int(input('enter starting number'))
+b=int(input('enter ending number'))
+while a < b:
+    if a % 2 == 0:
+        print(a)
+    a += 1
+            
